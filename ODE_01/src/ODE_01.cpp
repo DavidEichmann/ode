34673c4594a777c1beefad77b948f995604df01c
David Eichmann <EichmannD@gmail.com>
2013-11-14 19:13:26 +0000

    Addapting for use with OGRE

--- ODE_01/src/ODE_01.cpp
+++ ODE_01/src/ODE_01.cpp
@@ -6,29 +6,35 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+#include <string.h>
 #include <iostream>
 #include <ode/ode.h>
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <GL/freeglut.h>
+#include <OGRE/Ogre.h>
+#include <OIS/OIS.h>
 
 #include "Constants.h"
-
 
 dWorldID wid;
 dBodyID bid;
 dJointGroupID contactJointGroupID;
 dSpaceID space;
 
+Ogre::Root *mRoot;
+Ogre::Camera* mCamera;
+Ogre::SceneManager* mSceneMgr;
+Ogre::RenderWindow* mWindow;
+Ogre::SceneNode* sphereNode;
+
+
 void collisionCallback(void *data, dGeomID o1, dGeomID o2) {
 
 	// collect collision info
 	const int maxC = 10;
-	dContactGeom contact [maxC];
-	const int c = dCollide(o1,o2,maxC,contact,0);
+	dContactGeom contact[maxC];
+	const int c = dCollide(o1, o2, maxC, contact, 0);
 
 	// create collision joints
-	for(int i = 0; i < c; i++) {
+	for (int i = 0; i < c; i++) {
 		dContact dc;
 		dc.surface.mode = dContactBounce;
 		dc.surface.mu = 1;
@@ -46,34 +52,17 @@ void stepWorld() {
 	dSpaceCollide(space, NULL, collisionCallback);
 	//		Create a contact joint for every collision point, and put it in the contact joint group.
 	//		Take a simulation step.
-	dWorldStep(wid,STEP_SIZE);
+	dWorldStep(wid, STEP_SIZE);
 	//		Remove all joints in the contact joint group.
 
 }
 
-void renderScene() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glLoadIdentity();
-
-	glutSolidSphere(0.7,20,20);
-
-	glutSwapBuffers();
-}
-
-void keyHandler(unsigned char c, int a, int b) {
-	switch(c) {
-	case 27:
-		glutExit();
-		break;
-	}
-}
 void destroyWorld() {
 
 	//	Destroy the dynamics and collision worlds.
-	dWorldDestroy (wid);
+	dWorldDestroy(wid);
 
 }
-
 
 void initWorld() {
 
@@ -84,19 +73,19 @@ void initWorld() {
 	/// space
 	space = dHashSpaceCreate(0);
 	/// floor
-	dCreatePlane(space,0,1,0,0);
+	dCreatePlane(space, 0, 1, 0, 0);
 
 	//	Create bodies in the dynamics world.
 	/// simple sphere
 	bid = dBodyCreate(wid);
 	dGeomID gid = dCreateSphere(space, 1);
 	static dMass mass;
-	dMassSetBox(&mass,1,1,1,1);
+	dMassSetBox(&mass, 1, 1, 1, 1);
 	dBodySetMass(bid, &mass);
 	dGeomSetBody(gid, bid);
 
 	//	Set the state (position etc) of all bodies.
-	dBodySetPosition(bid,0,10,0);
+	dBodySetPosition(bid, 0, 10, 0);
 
 	//	Create joints in the dynamics world.
 
@@ -113,35 +102,70 @@ void initWorld() {
 
 int main(int pargc, char** argv) {
 
-	// initialize glut
-	glutInit(&pargc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(800, 800);
-	glutCreateWindow("My awesome 3D program");
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_LIGHTING);
+	// init ogre
+	/// Create root
+	mRoot = new Ogre::Root("", "", "");
 
-	// register callbacks
-	glutDisplayFunc(renderScene);
-	glutKeyboardFunc(keyHandler);
+	// Load the OpenGL RenderSystem and the Octree SceneManager plugins
+	std::string pluginPath = "/usr/lib/i386-linux-gnu/OGRE-1.7.4/";
+	mRoot->loadPlugin(pluginPath + "RenderSystem_GL");
+	mRoot->loadPlugin(pluginPath + "Plugin_OctreeSceneManager");
 
-	// lighting
-	glClearColor(1,1,1,1);
-	glEnable(GL_LIGHT0);
-	GLfloat light_point[] = { 1,1,-1, 0 };
-	glLightfv(GL_LIGHT0, GL_POSITION, light_point);
+	// configure the openGL / video settings
+	Ogre::RenderSystem* rs = mRoot->getRenderSystemByName("OpenGL Rendering Subsystem");
+	if(!(rs->getName() == "OpenGL Rendering Subsystem"))
+	{
+	    return false; //No RenderSystem found
+	}
+	// configure our RenderSystem
 
-	glEnable(GL_LIGHT1);
-	GLfloat light_ambient[] = { 0.3, 0.3, 0.3, 1.0 };
-	glLightfv(GL_LIGHT1, GL_AMBIENT, light_ambient);
+	//////////////// AAAAAAAAAAAAAAAAAa
+
+	rs->setConfigOption("Full Screen", "No");
+	rs->setConfigOption("VSync", "No");
+	std::string x = rs->getConfigOptions()["Video Mode"].currentValue;
+	rs->setConfigOption("Video Mode", rs->getConfigOptions()["Video Mode"].currentValue);
+	mRoot->setRenderSystem(rs);
+
+	/// create generic scene manager
+	mSceneMgr = mRoot->createSceneManager(Ogre::ST_GENERIC);
+	/// create camera
+	mCamera = mSceneMgr->createCamera("Cam");
+	mCamera->setPosition(Ogre::Vector3(0, 0, 80));
+	mCamera->lookAt(Ogre::Vector3(0, 5, 0));
+	mCamera->setNearClipDistance(5);
+
+	/// light
+	mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5));
+	Ogre::Light* l = mSceneMgr->createLight("MainLight");
+	l->setPosition(20,80,50);
+
+	/// create window/viewport
+	mWindow = mRoot->initialise(true, "My amazing simulation");
+	Ogre::Viewport* vp = mWindow->addViewport(mCamera);
+	vp->setBackgroundColour(Ogre::ColourValue(1, 1, 1));
+
+	/// Alter the camera aspect ratio to match the viewport
+	mCamera->setAspectRatio(
+			Ogre::Real(vp->getActualWidth())
+			/ Ogre::Real(vp->getActualHeight()));
+
+	/// Create sphere
+	Ogre::Entity* se = mSceneMgr->createEntity("My Sphere", Ogre::SceneManager::PT_SPHERE);
+	sphereNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("My Sphere Node");
+	sphereNode->attachObject(se);
 
 	// world contents
 	dInitODE();
 	initWorld();
 
 	// enter GLUT event processing cycle
-	glutMainLoop();
+	while( ! mWindow->isClosed()) {
+		stepWorld();
+		const float* pos = dBodyGetPosition(bid);
+		sphereNode->setPosition(pos[0], pos[1], pos[2]);
+		mRoot->renderOneFrame();
+	}
 
 	// destroy world
 	destroyWorld();
