c1351032fa3c1ec01367296fcaede7bbd3197a9d
David Eichmann <EichmannD@gmail.com>
2013-11-18 23:59:52 +0000

    Simulation is working to a degree. error on collision

---
+++ ODE_01/lib/ogre-procedural/samples/extrusion/CMakeFiles/Sample_Extrusion.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Sample_Extrusion.dir/__/common/src/BaseApplication.cpp.o"
+  "CMakeFiles/Sample_Extrusion.dir/src/Extrusion.cpp.o"
+  "../../bin/Sample_Extrusion.pdb"
+  "../../bin/Sample_Extrusion"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Sample_Extrusion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- ODE_01/src/ODE_01.cpp
+++ ODE_01/src/ODE_01.cpp
@@ -15,8 +15,9 @@
 #include "Constants.h"
 
 
+
 dWorldID wid;
-dBodyID bid;
+static dBodyID bid;
 dJointGroupID contactJointGroupID;
 dSpaceID space;
 
@@ -28,20 +29,29 @@ Ogre::SceneNode* sphereNode;
 
 
 void collisionCallback(void *data, dGeomID o1, dGeomID o2) {
+	// ignore collisions with spaces
+	if(dGeomIsSpace(o1) || dGeomIsSpace(o2)) {
+		return;
+	}
 
 	// collect collision info
 	const int maxC = 10;
 	dContactGeom contact[maxC];
-	const int c = dCollide(o1, o2, maxC, contact, (int)sizeof(dContact));
+	const int c = dCollide(o1, o2, maxC, contact, (int)sizeof(dContactGeom));
 
 	// create collision joints
 	for (int i = 0; i < c; i++) {
 		dContact dc;
+
 		dc.surface.mode = dContactBounce;
 		dc.surface.mu = 1;
 		dc.surface.bounce = 0.5;
+		dc.surface.bounce_vel = 0.1;
+
 		dJointID c = dJointCreateContact(wid, contactJointGroupID, &dc);
-		dJointAttach(c, dGeomGetBody(o1), dGeomGetBody(o2));
+		dBodyID b1 = dGeomGetBody(o1);
+		dBodyID b2 = dGeomGetBody(o2);
+		dJointAttach(c, b1, b2);
 	}
 }
 
@@ -50,7 +60,7 @@ void stepWorld() {
 	//		Apply forces to the bodies as necessary.
 	//		Adjust the joint parameters as necessary.
 	//		Call collision detection.
-//	dSpaceCollide(space, NULL, collisionCallback);
+	dSpaceCollide(space, NULL, collisionCallback);
 	//		Create a contact joint for every collision point, and put it in the contact joint group.
 	//		Take a simulation step.
 	dWorldStep(wid, STEP_SIZE);
@@ -79,9 +89,11 @@ void initWorld() {
 	//	Create bodies in the dynamics world.
 	/// simple sphere
 	bid = dBodyCreate(wid);
-	dGeomID gid = dCreateSphere(space, 1);
+	static dGeomID gid = dCreateSphere(space, 1);
 	static dMass mass;
-	dMassSetBox(&mass, 1, 1, 1, 1);
+	float fd = mass.mass;
+	dMassSetSphere(&mass, 1, 1);
+	fd = mass.mass;
 	dBodySetMass(bid, &mass);
 	dGeomSetBody(gid, bid);
 
@@ -116,6 +128,8 @@ int main(int pargc, char** argv) {
 	mRoot->loadPlugin(pluginPath + "Plugin_OctreeSceneManager");
 	// mRoot->loadPlugin(pluginPath + "Plugin_ParticleFX0");
 
+	mRoot->addResourceLocation("../Media", "FileSystem", "Group", true);
+
 	mRoot->restoreConfig() || mRoot->showConfigDialog();
 //	// configure the openGL / video settings
 //	Ogre::RenderSystem* rs = mRoot->getRenderSystemByName("OpenGL Rendering Subsystem");
@@ -142,10 +156,10 @@ int main(int pargc, char** argv) {
 
 	/// create camera
 	mCamera = mSceneMgr->createCamera("My Cam");
-	mCamera->setPosition(Ogre::Vector3(0, 0, -10));
+	mCamera->setPosition(Ogre::Vector3(0, 0, 1000));
 	mCamera->lookAt(Ogre::Vector3(0, 0, 0));
-	mCamera->setNearClipDistance(1);
-	mCamera->setFarClipDistance(1000);
+	mCamera->setNearClipDistance(5);
+	mCamera->setFarClipDistance(10000);
 
 	/// create window/viewport
 	Ogre::Viewport* vp = mWindow->addViewport(mCamera);
@@ -175,10 +189,11 @@ int main(int pargc, char** argv) {
 
 	// enter GLUT event processing cycle
 	while( ! mWindow->isClosed()) {
-//		stepWorld();
-//		const float* pos = dBodyGetPosition(bid);
-//		sphereNode->setPosition(pos[0], pos[1], pos[2]);
-		sphereNode->setPosition(0, 0, 0);
+		stepWorld();
+		const dReal* pos = dBodyGetPosition(bid);
+		dReal scale = 0.5;
+		sphereNode->setPosition(scale*pos[0], scale*pos[1], scale*pos[2]);
+		std::cout << pos[1] << std::endl;
 		Ogre::WindowEventUtilities::messagePump();
 		mRoot->renderOneFrame(10);
 	}
