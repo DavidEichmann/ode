ec64d1c1d1ba4c45a57ec0460130d70c5aea8da1
davideichmann <EichmannD@gmail.com>
2013-11-19 17:38:17 +0000

    Ball drop and bounce simulation and visualisation is working.

--- ODE_01/src/Constants.h
+++ ODE_01/src/Constants.h
@@ -1,2 +1,2 @@
 const dReal GRAVITY_ACC = -9.8;
-const dReal STEP_SIZE = 0.01; // in seconds
+const dReal STEP_SIZE = 0.1; // in seconds
--- ODE_01/src/ODE_01.cpp
+++ ODE_01/src/ODE_01.cpp
@@ -8,13 +8,13 @@
 
 #include <string.h>
 #include <iostream>
+#include <boost/chrono/system_clocks.hpp>
+#include <time.h>
 #include <ode/ode.h>
 #include <OGRE/Ogre.h>
 #include <OIS/OIS.h>
 
 #include "Constants.h"
-
-
 
 dWorldID wid;
 static dBodyID bid;
@@ -27,44 +27,59 @@ Ogre::SceneManager* mSceneMgr;
 Ogre::RenderWindow* mWindow;
 Ogre::SceneNode* sphereNode;
 
-
 void collisionCallback(void *data, dGeomID o1, dGeomID o2) {
+
 	// ignore collisions with spaces
-	if(dGeomIsSpace(o1) || dGeomIsSpace(o2)) {
+	if (dGeomIsSpace(o1) || dGeomIsSpace(o2)) {
 		return;
 	}
+
+	dBodyID b1 = dGeomGetBody(o1);
+	dBodyID b2 = dGeomGetBody(o2);
 
 	// collect collision info
 	const int maxC = 10;
 	dContactGeom contact[maxC];
-	const int c = dCollide(o1, o2, maxC, contact, (int)sizeof(dContactGeom));
+	const int c = dCollide(o1, o2, maxC, contact, (int) sizeof(dContactGeom));
 
 	// create collision joints
 	for (int i = 0; i < c; i++) {
+
 		dContact dc;
 
 		dc.surface.mode = dContactBounce;
 		dc.surface.mu = 1;
 		dc.surface.bounce = 0.5;
 		dc.surface.bounce_vel = 0.1;
+		dc.geom = contact[i];
 
-		dJointID c = dJointCreateContact(wid, contactJointGroupID, &dc);
-		dBodyID b1 = dGeomGetBody(o1);
-		dBodyID b2 = dGeomGetBody(o2);
-		dJointAttach(c, b1, b2);
+		dJointID cj = dJointCreateContact(wid, contactJointGroupID, &dc);
+		dJointAttach(cj, b1, b2);
 	}
 }
 
-void stepWorld() {
+/**
+ * dt: target simulation time
+ * returns the time actually simulated
+ */
+double stepWorld(double dt) {
 
-	//		Apply forces to the bodies as necessary.
-	//		Adjust the joint parameters as necessary.
-	//		Call collision detection.
-	dSpaceCollide(space, NULL, collisionCallback);
-	//		Create a contact joint for every collision point, and put it in the contact joint group.
-	//		Take a simulation step.
-	dWorldStep(wid, STEP_SIZE);
-	//		Remove all joints in the contact joint group.
+	//		Take simulation step (consists of potentially many ode world steps).
+	double timeSim = 0;
+	while (timeSim < dt) {
+		//		Apply forces to the bodies as necessary.
+		//		Adjust the joint parameters as necessary.
+		//		Call collision detection.
+		dSpaceCollide(space, NULL, collisionCallback);
+		//		Create a contact joint for every collision point, and put it in the contact joint group.
+		dWorldStep(wid, STEP_SIZE);
+		timeSim += STEP_SIZE;
+
+		//		Remove all joints in the contact joint group.
+		dJointGroupEmpty(contactJointGroupID);
+	}
+
+	return timeSim;
 
 }
 
@@ -91,14 +106,12 @@ void initWorld() {
 	bid = dBodyCreate(wid);
 	static dGeomID gid = dCreateSphere(space, 1);
 	static dMass mass;
-	float fd = mass.mass;
 	dMassSetSphere(&mass, 1, 1);
-	fd = mass.mass;
 	dBodySetMass(bid, &mass);
 	dGeomSetBody(gid, bid);
 
 	//	Set the state (position etc) of all bodies.
-	dBodySetPosition(bid, 0, 10, 0);
+	dBodySetPosition(bid, 0, 50, 0);
 
 	//	Create joints in the dynamics world.
 
@@ -120,7 +133,7 @@ int main(int pargc, char** argv) {
 	mRoot = new Ogre::Root("", "ogre.cfg", "ogre.log");
 
 	// Load the OpenGL RenderSystem and the Octree SceneManager plugins
-	std::string pluginPath = "/usr/lib/i386-linux-gnu/OGRE-1.8.0/";
+	std::string pluginPath = "/usr/lib/x86_64-linux-gnu/OGRE-1.8.0/";
 	mRoot->loadPlugin(pluginPath + "RenderSystem_GL");
 	mRoot->loadPlugin(pluginPath + "Plugin_BSPSceneManager");
 	mRoot->loadPlugin(pluginPath + "Plugin_PCZSceneManager");
@@ -156,8 +169,8 @@ int main(int pargc, char** argv) {
 
 	/// create camera
 	mCamera = mSceneMgr->createCamera("My Cam");
-	mCamera->setPosition(Ogre::Vector3(0, 0, 1000));
-	mCamera->lookAt(Ogre::Vector3(0, 0, 0));
+	mCamera->setPosition(Ogre::Vector3(0, 300, 1000));
+	mCamera->lookAt(Ogre::Vector3(0, 300, 0));
 	mCamera->setNearClipDistance(5);
 	mCamera->setFarClipDistance(10000);
 
@@ -167,33 +180,49 @@ int main(int pargc, char** argv) {
 	// Alter the camera aspect ratio to match the viewport
 	mCamera->setAspectRatio(
 			Ogre::Real(vp->getActualWidth())
-			/ Ogre::Real(vp->getActualHeight()));
-
+					/ Ogre::Real(vp->getActualHeight()));
 
 	Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
 
 	/// light
 	mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5));
 	Ogre::Light* l = mSceneMgr->createLight("MainLight");
-	l->setPosition(20,80,50);
+	l->setPosition(20, 80, 50);
 
 	/// Create sphere
-	Ogre::Entity* se = mSceneMgr->createEntity("My Sphere", Ogre::SceneManager::PT_SPHERE);
-	sphereNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("My Sphere Node");
+	Ogre::Entity* se = mSceneMgr->createEntity("My Sphere",
+			Ogre::SceneManager::PT_SPHERE);
+	sphereNode = mSceneMgr->getRootSceneNode()->createChildSceneNode(
+			"My Sphere Node");
 	sphereNode->attachObject(se);
-
 
 	// world contents
 	dInitODE();
 	initWorld();
 
-	// enter GLUT event processing cycle
-	while( ! mWindow->isClosed()) {
-		stepWorld();
+	// setup timing variables
+	boost::chrono::system_clock::time_point it,ft; 	// initial time, final time (between frames)
+	ft = boost::chrono::system_clock::now();
+	double dt;		// time delta of the frame
+	double simTOffset = 0;	// real time (seconds) - time in the simulation
+	// mian loop
+	while (!mWindow->isClosed()) {
+		// get the time and time delta
+		it = ft;
+		ft = boost::chrono::system_clock::now();
+		dt = ((boost::chrono::nanoseconds) (ft - it)).count() / ((double) 1000000000);
+		simTOffset += dt;
+
+
+		// step the world
+		simTOffset -= stepWorld(simTOffset);
+
+		// update the position of the sphere (from ODE world to OGRE world)
 		const dReal* pos = dBodyGetPosition(bid);
-		dReal scale = 0.5;
-		sphereNode->setPosition(scale*pos[0], scale*pos[1], scale*pos[2]);
-		std::cout << pos[1] << std::endl;
+		dReal scale = 7;
+		sphereNode->setPosition(scale * pos[0], scale * pos[1], scale * pos[2]);
+
+		// Render the world
 		Ogre::WindowEventUtilities::messagePump();
 		mRoot->renderOneFrame(10);
 	}
